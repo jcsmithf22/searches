e3b85c068b492612dbe20bad8a94d3a8f5905fbe
Josiah Smith <jcsmithf22@gmail.com>
2025-11-12 18:36:08 -0600

    run authentication migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_13_000258) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_13_003545) do
   create_table "searches", force: :cascade do |t|
     t.string "buying_options"
     t.string "category_ids"
@@ -30,4 +30,23 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_13_000258) do
     t.text "sellers"
     t.datetime "updated_at", null: false
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
