707f47f4b1075003150c3da7b367e1ab2ac443f5
Josiah Smith <jcsmithf22@gmail.com>
2025-12-06 22:16:39 -0600

    get turbo stream working

--- app/controllers/searches/pins_controller.rb
+++ app/controllers/searches/pins_controller.rb
@@ -4,8 +4,10 @@ class Searches::PinsController < ApplicationController
   def update
     if @search.pinned_by?(Current.user)
       @search.unpin_by(Current.user)
+      @pinned = false
     else
       @search.pin_by(Current.user)
+      @pinned = true
     end
 
     respond_to do |format|
